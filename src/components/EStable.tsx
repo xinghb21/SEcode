32eae0a19e361ab91a0156a7b1a2f3de015df04f
陈俊廷 <2021011051@secoder.net>
2023-03-28 15:54:38 +0800

    [F-1-0-NN-003]完善侧边栏应用获取方式(#3)

--- src/components/EStable.tsx
+++ src/components/EStable.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import CreateES from "./createES";
 import UserTable from "./Table";
-import React from 'react';
-import { Button } from 'antd';
+import React from "react";
+import { Button } from "antd";
 
 interface User{
   id:number;
@@ -12,20 +12,20 @@ interface User{
 }
 
 const EStable=()=> {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [users, setUsers] = useState<User[]>([]);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const handleCreateUser = (user: User) => {
-    setUsers([...users, user]);
-    setIsDialogOpen(false);
-  };
+    const handleCreateUser = (user: User) => {
+        setUsers([...users, user]);
+        setIsDialogOpen(false);
+    };
 
-  return (
-    <>
-      <Button onClick={() => setIsDialogOpen(true)}>创建企业系统管理员</Button>
-      <UserTable users={users} width={100} height={100}/>
-      <CreateES isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} onCreateUser={handleCreateUser} />
-    </>
-  );
-}
+    return (
+        <>
+            <Button onClick={() => setIsDialogOpen(true)}>创建企业系统管理员</Button>
+            <UserTable users={users} width={100} height={100}/>
+            <CreateES isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} onCreateUser={handleCreateUser} />
+        </>
+    );
+};
 export default EStable;
