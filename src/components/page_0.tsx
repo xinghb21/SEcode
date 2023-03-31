fe177de51b1e8d371396fdbe68b5f8b83de3fd70
陈俊廷 <2021011051@secoder.net>
2023-03-31 17:37:25 +0800

    [F-2-1-NN-005]处理page0.tsx报错 (#5)

--- src/components/page_0.tsx
+++ src/components/page_0.tsx
@@ -2,7 +2,7 @@ import { Button , Table} from "antd";
 import { useRouter } from "next/router";
 import type { ColumnsType } from "antd/es/table";
 import React, { useEffect, useState } from "react";
-import { request } from "../../utils/network";
+import { request } from "../utils/network";
 
 interface Entity {
     key: React.Key;
