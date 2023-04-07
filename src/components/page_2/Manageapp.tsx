134fd15742525b818cd3b1449d4d6783c4beda6b
何青峰 <2021010835@secoder.net>
2023-04-07 18:25:09 +0800

    [F-2-0-ES-008]完成系统管理员人员管理界面的代码框架(#8)

---
+++ src/components/page_2/Manageapp.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Modal, Input } from "antd";
+import React from "react";
+
+interface EntityRegister{
+    key: React.Key;
+    entityname:string;
+  }
+
+interface DialogProps{
+    isOpen: boolean;
+    onClose: () => void;
+    onCreateUser: (entity:EntityRegister ) => void;
+  }
+
+const Manageapp=(props: DialogProps) =>{
+    const [entity, setEntity] = useState<string>("");
+
+    const handleCreateUser = () => {
+        const newentity: EntityRegister = {
+            key: Date.now(),
+            entityname:entity,
+        };
+        props.onCreateUser(newentity);
+        setEntity("");
+    };
+
+    return (
+        <Modal  title="创建业务实体" open={props.isOpen} onOk={handleCreateUser} onCancel={props.onClose} >
+            <div>
+                <label>业务实体名称:</label>
+                <Input type="text" value={entity} onChange={(e) => setEntity(e.target.value)} />
+            </div>
+        </Modal>
+    );
+};
+export default Manageapp;
