3727444f1d842c4e1512100b35bc5d81e0357ed6
邢皓斌 <2021010823@secoder.net>
2023-04-18 15:28:22 +0000

    [F-3-0-EP-017]修改页面刷新(#017)

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -80,7 +80,7 @@ const User: React.FC = () => {
                 setName(res.name);
                 fetchList(res.name);
             });
-    }, []);
+    }, [router]);
 
     //通过后端获取的funlist以及用户对应的identity实现侧边栏应用
     //具体的key还需要完善
