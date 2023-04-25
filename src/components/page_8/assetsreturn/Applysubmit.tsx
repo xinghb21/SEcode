4f2513a1da1e09e4db87e88a0c038dd283d3267a
何青峰 <2021010835@secoder.net>
2023-04-25 11:36:27 +0800

    [F-3-0-NN-021]完成了资产退库(#21)

---
+++ src/components/page_8/assetsreturn/Applysubmit.tsx
@@ -0,0 +1,96 @@
+import { Avatar, List, Space, Button, Tag, message, Modal, Input } from "antd";
+import React from "react";
+import { ProForm, ProFormDatePicker, ProFormSelect, ProFormText, ProList, QueryFilter, hrHRIntl } from "@ant-design/pro-components";
+import { Progress } from "antd";
+import type { ReactText } from "react";
+import { useState } from "react";
+import {useEffect} from "react";
+import { request } from "../../../utils/network";
+interface DialogProps{
+
+    isOpen: boolean;
+    onClose: ()=>void;
+    onSuccess:()=> void;
+    proassetlist:asset[];
+}
+interface asset{
+    key:React.Key;
+    id:number;
+    name:string;
+    type:number;
+    count:number;
+    applycount:number;
+}
+
+const Applysubmit=(props:DialogProps)=>{
+    const [reason,setreason]=useState<string>("");
+    const [loading,setloading]=useState<boolean>(false);
+    const handlesubmit=()=>{
+        setloading(true);
+        if(reason!==""){
+            request("api/user/ns/returnasset","POST",{assets:props.proassetlist.map((val)=>{return{id:val.id,assetname:val.name,assetnumber:val.applycount};}),reason:reason})
+                .then((res)=>{
+                    message.success("提交成功，请等待审批");
+                    props.onClose();
+                    props.onSuccess();
+                    setloading(false);
+                })
+                .catch((err)=>{
+                //申请没成功就关闭页面
+                    message.warning(err.message);
+                    props.onClose();
+                    setloading(false);
+                });    
+        }else{
+            message.warning("请填写申请原因");
+            setloading(false);
+        }
+    };
+    return (
+        <Modal  title="资产退库申请" onOk={()=>{handlesubmit();}} okText={"提交申请"} confirmLoading={loading} onCancel={props.onClose} open={props.isOpen}  >
+            <label>请填写申请原因：</label>
+            <Input type='text' onChange={(e)=>{setreason(e.target.value);}} maxLength={200}></Input>
+            <ProList<asset>
+                pagination={{
+                    pageSize: 6,
+                }}
+                metas={
+                    {
+                        title: {dataIndex:"name"},
+                        description: {
+                            render: (_,row) => {
+                                return (
+                                    <div>
+                                        <div>
+                                            {"退库数量: "+row.count}
+                                        </div>
+                                        <div>
+                                            {"资产编号："+row.id}
+                                        </div>
+                                    </div>
+                                );
+                            },
+                        },
+                        subTitle: {
+                            render: (_, row) => {
+                                return (
+                                    <Space size={0}>
+                                        {(row.type===1)?<Tag color="blue" key={row.name}>{"数量型"}</Tag>
+                                            :<Tag color="blue" key={row.name}>{"条目型"}</Tag>  
+                                        }
+                                    </Space>
+                                );
+                            },
+                            search: false,
+                        },
+                    }
+                }
+                rowKey="key"
+                headerTitle="本次退库的所有资产"
+                dataSource={props.proassetlist}
+            />
+        </Modal>
+    );
+};
+
+export default Applysubmit;
