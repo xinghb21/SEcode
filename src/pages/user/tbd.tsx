42cb15207394f81d48435fb7a95433997918c7ae
陈俊廷 <2021011051@secoder.net>
2023-04-27 10:23:23 +0800

    [F-3-0-EP-019]更改tbd关闭组件(#19)

--- src/pages/user/tbd.tsx
+++ src/pages/user/tbd.tsx
@@ -247,7 +247,7 @@ const TbdDrawer = () => {
                 <Modal
                     open={open}
                     title="该员工所申请资产详细"
-                    onOk={handleCancel}
+                    onCancel={handleCancel}
                     footer={[
                         <Button key="back" onClick={handleCancel}>
                             关闭
