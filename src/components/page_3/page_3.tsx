00f40842c7da5a251ea360f0ca3d7d6ef18e31ee
何青峰 <2021010835@secoder.net>
2023-05-03 18:46:55 +0800

    [F-3-0-NN-021]initial before pull dev(#21)

--- src/components/page_3/page_3.tsx
+++ src/components/page_3/page_3.tsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { Avatar, List, Space, Button, Tag, message } from "antd";
+import { ProForm, ProFormDatePicker, ProFormSelect, ProFormText, ProList, QueryFilter, hrHRIntl } from "@ant-design/pro-components";
+import { Progress } from "antd";
+import type { ReactText } from "react";
+import { useState } from "react";
+import { BUILD_ID_FILE } from "next/dist/shared/lib/constants";
+import {useEffect} from "react";
+import { request } from "../../utils/network";
+import { Md5 } from "ts-md5";
+import Column from "antd/es/table/Column";
+interface log{
+    key:React.Key,
+    id:number,
+    
+}
 const Page_3 = () => {
+    
     return (
-        <div>
-            page: 3
-        </div>
+
     );
 };
 
